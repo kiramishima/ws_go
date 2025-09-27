06fb3709b9c8a53f7ead5b3ba0f2f9d3d8222a31
Kira Mishima <kiramishima@outlook.com>
2025-09-27 14:23:16 -0600

    Server & HTTP test

--- Courses/Pluralsight/Testing in Go/Introduction/user/component_test.go
+++ Courses/Pluralsight/Testing in Go/Introduction/user/component_test.go
@@ -3,7 +3,9 @@ package user
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -51,6 +53,77 @@ func TestHandler(t *testing.T) {
 	if !bytes.Equal(expect, got) {
 		t.Fail()
 	}
+}
+
+func TestHandler_httptest(t *testing.T) {
+	users = []User{
+		{
+			ID:       1,
+			Username: "adent",
+		},
+		{
+			ID:       2,
+			Username: "tmacmillan",
+		},
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/users", nil)
+	rec := httptest.NewRecorder()
+	expect, err := json.Marshal(users)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	Handler(rec, req)
+
+	res := rec.Result()
+	got, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// assert
+	if !bytes.Equal(expect, got) {
+		t.Fail()
+	}
+}
+
+func TestHandler_server(t *testing.T) {
+	users = []User{
+		{
+			ID:       1,
+			Username: "adent",
+		},
+		{
+			ID:       2,
+			Username: "tmacmillan",
+		},
+	}
+	expect, err := json.Marshal(users)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// creamos el servidor
+	s := httptest.NewServer(http.HandlerFunc(Handler))
+	// configuramos un cliente
+	c := s.Client()
+	// hacemos la petición
+	res, err := c.Get(s.URL + "/users")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// leemos la respuesta
+	got, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// assert
+	if !bytes.Equal(expect, got) {
+		t.Fail()
+	}
+	// cerramos el servidor
+	s.Close()
 }
 
 func BenchmarkHandler(b *testing.B) {
